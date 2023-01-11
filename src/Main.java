13f19b3540e49c42527d439dd36d461673df3528
Evgeny Fisenko <evgeny.f.aircelle@gmail.com>
2023-01-11 16:16:31 +0200

    решение задачи билет № 4

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "Hello, World! My name is Java.";
+        String[] arr = str.split(" ");
+        for (String s : arr) {
+            System.out.println(s);
+
+        }
+    }
+}
